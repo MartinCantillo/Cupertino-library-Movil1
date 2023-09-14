34eaa16f78066cfee0fb464ef8c214408d6473d1
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-13 23:01:45 -0500

    Recents

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [CupertinoTabBar].
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
       theme: CupertinoThemeData(brightness: Brightness.dark),
-      home: CupertinoTabBarExample(),
+      home: CupertinoTabBarr(),
     );
   }
 }
 
-class CupertinoTabBarExample extends StatelessWidget {
-  const CupertinoTabBarExample({super.key});
+class CupertinoTabBarr extends StatefulWidget {
+  const CupertinoTabBarr({Key? key}) : super(key: key);
+
+  @override
+  _CupertinoTabBarState createState() => _CupertinoTabBarState();
+}
+
+class _CupertinoTabBarState extends State<CupertinoTabBarr> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -48,18 +53,22 @@ class CupertinoTabBarExample extends StatelessWidget {
           builder: (BuildContext context) {
             if (index == 0) {
               return CupertinoPageScaffold(
-                 navigationBar: CupertinoNavigationBar(
+                navigationBar: CupertinoNavigationBar(
                   middle: Text('Favorites'),
-                  leading:  Icon(CupertinoIcons.add),
+                  leading: Icon(CupertinoIcons.add),
                 ),
                 child: Center(
                   child: Text('No favorites yet'),
                 ),
               );
-              ;
             } else if (index == 1) {
-              return Center(
-                child: Text('pestaña 2'),
+              return CupertinoPageScaffold(
+                navigationBar: CupertinoNavigationBar(
+                  middle: Text('Recents'),
+                ),
+                child: Center(
+                  child: CallList(),
+                ),
               );
             } else if (index == 2) {
               return Center(
@@ -74,6 +83,48 @@ class CupertinoTabBarExample extends StatelessWidget {
               child: Text('Pestaña no reconocida'),
             );
           },
+        );
+      },
+    );
+  }
+}
+
+
+//make call
+class Call {
+  final String contactName;
+  final String dateTime;
+
+  Call(this.contactName, this.dateTime);
+}
+
+
+class CallList extends StatelessWidget {
+  const CallList({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    // list
+    final List<Call> calls = [
+      Call("Daniel Rivera", "Today, 10:30 AM"),
+      Call("Diomedez Diaz", "yesterday, 5:45 PM"),
+      Call("Antonio eliseo", "yesterday, 3:15 PM"),
+      Call("Andrea ", "yesterday, 10:15 PM"),
+     Call("Carol G ", "Today, 3:50 PM"),
+     Call("Juaco pertuz ", "yesterday, 6:50 PM"),
+     Call("Oscar Gamarra", "Today, 11:20 AM"),
+    ];
+
+    return ListView.builder(
+      itemCount: calls.length,
+      itemBuilder: (context, index) {
+        final call = calls[index];
+        return CupertinoListTile(
+          title: Text(call.contactName),
+          subtitle: Text(call.dateTime),
+          leading: Icon(Icons.call), 
+          //trailing: Icon(Icons.cancel), 
+       
         );
       },
     );
